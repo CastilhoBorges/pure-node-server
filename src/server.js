928e563bb73428eb763834cc6acfbe68f8da80b0
Augusto Castilho Borges <augustocastilhoborges21@gmail.com>
2025-04-16 16:35:04 -0300

    removing comments

--- src/server.js
+++ src/server.js
@@ -1,9 +1,3 @@
-// Padrão de importação CommonJS
-// const http = require("http");
-
-// ESModules
-// Para usar no node nativamente, precisamos definir type:modules no package.json
-// Usamos node:module para definir que é um module interno do node
 import http from "node:http";
 import { routesTerminal } from "./routes/routes-terminal.js";
 
